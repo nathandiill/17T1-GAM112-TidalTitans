d2b2ec49dda4802b74c6952bab37cef0584fda86
DWM7 <davidmaitland777@gmail.com>
2017-04-13 13:18:11 +1000

    Working on android, all the models are added in for enemies, basic wave spawning system works, main menu and credits screen works, all towers placeable

---
+++ Tower Defense Test/Assets/Scripts/wavespawner.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class wavespawner : MonoBehaviour {
+
+    public GameObject DiverPrefab;
+    public GameObject CagePrefab;
+    public GameObject BossPrefab;
+    public Transform spawnpoint;
+    public float timebetweenwaves = 5f;
+    private float countdown = 2f;
+    private int wavenumber = 0;
+    public Text WaveText;
+    public GameObject Manager = GameObject.Find("GameMaster");
+    public Component GameManagerClass;
+
+
+    void Update()
+    {
+        if(countdown <= 0)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timebetweenwaves;
+
+        }
+        countdown -= Time.deltaTime;
+        WaveText.text = "Wave: " + wavenumber.ToString();
+    }
+
+    IEnumerator SpawnWave()
+    {
+        wavenumber++;
+        for (int i = 0; i < wavenumber; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+            if(wavenumber >= 5)
+            {
+                SpawnCage();
+                yield return new WaitForSeconds(0.8f);
+            }
+            if(wavenumber == 10)
+            {
+                Instantiate(BossPrefab, spawnpoint.position, spawnpoint.rotation);
+            }
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(DiverPrefab, spawnpoint.position, spawnpoint.rotation);
+        //GameManagerClass += 1;
+    }
+    void SpawnCage()
+    {
+        Instantiate(CagePrefab, spawnpoint.position, spawnpoint.rotation);
+    }
+    //void SpawnBoss()
+    //{
+    //    Instantiate(BossPrefab, spawnpoint.position, spawnpoint.rotation);
+    //}
+}
